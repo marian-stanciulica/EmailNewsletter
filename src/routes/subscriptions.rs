62b2d7752bd982f02db376f212e50f4f30e89cb1
Marian25 <marian.stanciulica@gmail.com>
2024-10-01 11:41:58 +0300

    Finish email validation

--- src/routes/subscriptions.rs
+++ src/routes/subscriptions.rs
@@ -2,12 +2,22 @@ use actix_web::{web, HttpResponse};
 use chrono::Utc;
 use sqlx::PgPool;
 use uuid::Uuid;
-use crate::domain::{NewSubscriber, SubscriberName};
+use crate::domain::{NewSubscriber, SubscriberEmail, SubscriberName};
 
 #[derive(serde::Deserialize)]
 pub struct FormData {
     email: String,
     name: String,
+}
+
+impl TryFrom<FormData> for NewSubscriber {
+    type Error = String;
+
+    fn try_from(value: FormData) -> Result<Self, Self::Error> {
+        let name = SubscriberName::parse(value.name)?;
+        let email = SubscriberEmail::parse(value.email)?;
+        Ok(NewSubscriber { email, name })
+    }
 }
 
 #[tracing::instrument(
@@ -19,13 +29,9 @@ pub struct FormData {
     )
 )]
 pub async fn subscribe(form: web::Form<FormData>, pool: web::Data<PgPool>) -> HttpResponse {
-    let name = match SubscriberName::parse(form.0.name) {
+    let new_subscriber = match form.0.try_into() {
         Ok(name) => name,
         Err(_) => return HttpResponse::BadRequest().finish(),
-    };
-    let new_subscriber = NewSubscriber {
-        email: form.0.email,
-        name: name
     };
     match insert_subscriber(&pool, new_subscriber).await {
         Ok(_) => HttpResponse::Ok().finish(),
@@ -44,7 +50,7 @@ pub async fn insert_subscriber(pool: &PgPool, new_subscriber: NewSubscriber) -> 
         VALUES ($1, $2, $3, $4)
         "#,
         Uuid::new_v4(),
-        new_subscriber.email,
+        new_subscriber.email.as_ref(),
         new_subscriber.name.as_ref(),
         Utc::now()
     )
