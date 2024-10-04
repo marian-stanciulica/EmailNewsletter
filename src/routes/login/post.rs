1850946e0f3d8fc693941ff2e727f484ff8f892b
Marian25 <marian.stanciulica@gmail.com>
2024-10-04 15:44:28 +0300

    Extracts authentication module from newsletters module

---
+++ src/routes/login/post.rs
@@ -0,0 +1,16 @@
+use actix_web::HttpResponse;
+use actix_web::http::header::LOCATION;
+use actix_web::web;
+use secrecy::Secret;
+
+#[derive(serde::Deserialize)]
+pub struct FormData {
+    _username: String,
+    _password: Secret<String>,
+}
+
+pub async fn login(_form: web::Form<FormData>) -> HttpResponse {
+    HttpResponse::SeeOther()
+        .insert_header((LOCATION, "/"))
+        .finish()
+}
--- src/startup.rs
+++ src/startup.rs
@@ -1,6 +1,6 @@
 use crate::configuration::{DatabaseSettings, Settings};
 use crate::email_client::EmailClient;
-use crate::routes::health_check;
+use crate::routes::{health_check, home, login, login_form};
 use crate::routes::subscribe;
 use crate::routes::{confirm, publish_newsletter};
 use actix_web::dev::Server;
@@ -80,6 +80,9 @@ fn run(
             .route("/subscriptions", web::post().to(subscribe))
             .route("/subscriptions/confirm", web::get().to(confirm))
             .route("/newsletters", web::post().to(publish_newsletter))
+            .route("/", web::get().to(home))
+            .route("/login", web::get().to(login_form))
+            .route("/login", web::post().to(login))
             .app_data(db_pool.clone())
             .app_data(email_client.clone())
             .app_data(base_url.clone())
