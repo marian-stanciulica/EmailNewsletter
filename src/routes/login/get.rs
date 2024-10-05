39779f561f352caa238bb626daaa56c2370cfb69
Marian25 <marian.stanciulica@gmail.com>
2024-10-05 12:00:18 +0300

    Deletes auth failed cookie after is shown

--- src/routes/login/get.rs
+++ src/routes/login/get.rs
@@ -1,12 +1,13 @@
 use actix_web::http::header::ContentType;
 use actix_web::{HttpRequest, HttpResponse};
+use actix_web::cookie::Cookie;
 
 pub async fn login_form(request: HttpRequest) -> HttpResponse {
     let error_html = match request.cookie("_flash") {
         None => "".into(),
         Some(cookie) => format!("<p><i>{}</i></p>", cookie.value())
     };
-    HttpResponse::Ok()
+    let mut response = HttpResponse::Ok()
         .content_type(ContentType::html())
         .body(format!(
             r#"<!DOCTYPE html>
@@ -36,5 +37,7 @@ pub async fn login_form(request: HttpRequest) -> HttpResponse {
                 </form>
             </body>
             </html>"#,
-        ))
+        ));
+    response.add_removal_cookie(&Cookie::new("_flash", "")).unwrap();
+    response
 }
